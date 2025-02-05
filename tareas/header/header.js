a5bd7ea0a7adcbe4c35adcca4c37310fbde2c88d
keneth <keneth.javier90@gmail.com>
2025-02-04 18:50:03 -0600

    se agregan los cambios a la rama header

--- tareas/header/header.js
+++ tareas/header/header.js
@@ -1,6 +1,22 @@
-// header.js
 function header() {
-    let header = document.createElement("header"); // Crea un elemento <header>
+    let header = document.createElement("header");
+    header.className = "header";
+
+    let contenedorImagen = document.createElement("div");
+    contenedorImagen.className = "header-image";
+
+    let imagen = document.createElement("img");
+    imagen.src = "https://w7.pngwing.com/pngs/227/599/png-transparent-luffy-flag.png";
+    imagen.alt = "Logo";
+    contenedorImagen.appendChild(imagen);
+
+    let contenedorTexto = document.createElement("div");
+    contenedorTexto.className = "header-text";
+    contenedorTexto.textContent = "todo-list";
+
+    header.appendChild(contenedorImagen);
+    header.appendChild(contenedorTexto);
+
     return header;
 }
 
